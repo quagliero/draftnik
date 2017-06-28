9d24db0b5f1aea2bf5e6a7dcaf05f091440d0250
Tobias <tobiaswhd@gmail.com>
2017-06-28 22:13:36 +0100

    Tidy Dodds' calculator, make it scale
    Tidy Dodds' calculator, make it scale

--- src/utils/dodds-calculator.js
+++ src/utils/dodds-calculator.js
@@ -1,99 +1,28 @@
 /* eslint-disable */
-function calculate(obj) {
+function calculate(givingPicks, receivingPicks) {
   /* Footballguys.com Quik-Vu™ Pick Value Calculator is Copyright © 1997 David Dodds. */
-  const pick1lose = ((obj.lose1.value * obj.lose1.value * obj.lose1.value * obj.lose1.value * obj.lose1.value * obj.lose1.value * 0.000000001059022) - (obj.lose1.value * obj.lose1.value * obj.lose1.value * obj.lose1.value * obj.lose1.value * 0.0000007143797) + (obj.lose1.value * obj.lose1.value * obj.lose1.value * obj.lose1.value * 0.0001881737) - (obj.lose1.value * obj.lose1.value * obj.lose1.value * 0.02460543) + (obj.lose1.value * obj.lose1.value * 1.724025) - (obj.lose1.value * 71.53730));
+  const getValue = (pick) => {
+    const magic = 1959.184;
+    const value = (pick.overall * pick.overall * pick.overall * pick.overall * pick.overall * pick.overall * 0.000000001059022)
+    - (pick.overall * pick.overall * pick.overall * pick.overall * pick.overall * 0.0000007143797)
+    + (pick.overall * pick.overall * pick.overall * pick.overall * 0.0001881737)
+    - (pick.overall * pick.overall * pick.overall * 0.02460543)
+    + (pick.overall * pick.overall * 1.724025)
+    - (pick.overall * 71.53730);
 
-  const pick2lose = ((obj.lose2.value * obj.lose2.value * obj.lose2.value * obj.lose2.value * obj.lose2.value * obj.lose2.value * 0.000000001059022) - (obj.lose2.value * obj.lose2.value * obj.lose2.value * obj.lose2.value * obj.lose2.value * 0.0000007143797) + (obj.lose2.value * obj.lose2.value * obj.lose2.value * obj.lose2.value * 0.0001881737) - (obj.lose2.value * obj.lose2.value * obj.lose2.value * 0.02460543) + (obj.lose2.value * obj.lose2.value * 1.724025) - (obj.lose2.value * 71.53730));
+    if (value < 0) {
+      return Math.round(value + magic);
+    }
 
-  const pick3lose = ((obj.lose3.value * obj.lose3.value * obj.lose3.value * obj.lose3.value * obj.lose3.value * obj.lose3.value * 0.000000001059022) - (obj.lose3.value * obj.lose3.value * obj.lose3.value * obj.lose3.value * obj.lose3.value * 0.0000007143797) + (obj.lose3.value * obj.lose3.value * obj.lose3.value * obj.lose3.value * 0.0001881737) - (obj.lose3.value * obj.lose3.value * obj.lose3.value * 0.02460543) + (obj.lose3.value * obj.lose3.value * 1.724025) - (obj.lose3.value * 71.53730));
-
-  const pick4lose = ((obj.lose4.value * obj.lose4.value * obj.lose4.value * obj.lose4.value * obj.lose4.value * obj.lose4.value * 0.000000001059022) - (obj.lose4.value * obj.lose4.value * obj.lose4.value * obj.lose4.value * obj.lose4.value * 0.0000007143797) + (obj.lose4.value * obj.lose4.value * obj.lose4.value * obj.lose4.value * 0.0001881737) - (obj.lose4.value * obj.lose4.value * obj.lose4.value * 0.02460543) + (obj.lose4.value * obj.lose4.value * 1.724025) - (obj.lose4.value * 71.53730));
-
-  const pick5lose = ((obj.lose5.value * obj.lose5.value * obj.lose5.value * obj.lose5.value * obj.lose5.value * obj.lose5.value * 0.000000001059022) - (obj.lose5.value * obj.lose5.value * obj.lose5.value * obj.lose5.value * obj.lose5.value * 0.0000007143797) + (obj.lose5.value * obj.lose5.value * obj.lose5.value * obj.lose5.value * 0.0001881737) - (obj.lose5.value * obj.lose5.value * obj.lose5.value * 0.02460543) + (obj.lose5.value * obj.lose5.value * 1.724025) - (obj.lose5.value * 71.53730));
-
-  const pick1gain = ((obj.gain1.value * obj.gain1.value * obj.gain1.value * obj.gain1.value * obj.gain1.value * obj.gain1.value * 0.000000001059022) - (obj.gain1.value * obj.gain1.value * obj.gain1.value * obj.gain1.value * obj.gain1.value * 0.0000007143797) + (obj.gain1.value * obj.gain1.value * obj.gain1.value * obj.gain1.value * 0.0001881737) - (obj.gain1.value * obj.gain1.value * obj.gain1.value * 0.02460543) + (obj.gain1.value * obj.gain1.value * 1.724025) - (obj.gain1.value * 71.53730));
-
-  const pick2gain = ((obj.gain2.value * obj.gain2.value * obj.gain2.value * obj.gain2.value * obj.gain2.value * obj.gain2.value * 0.000000001059022) - (obj.gain2.value * obj.gain2.value * obj.gain2.value * obj.gain2.value * obj.gain2.value * 0.0000007143797) + (obj.gain2.value * obj.gain2.value * obj.gain2.value * obj.gain2.value * 0.0001881737) - (obj.gain2.value * obj.gain2.value * obj.gain2.value * 0.02460543) + (obj.gain2.value * obj.gain2.value * 1.724025) - (obj.gain2.value * 71.53730));
-
-  const pick3gain = ((obj.gain3.value * obj.gain3.value * obj.gain3.value * obj.gain3.value * obj.gain3.value * obj.gain3.value * 0.000000001059022) - (obj.gain3.value * obj.gain3.value * obj.gain3.value * obj.gain3.value * obj.gain3.value * 0.0000007143797) + (obj.gain3.value * obj.gain3.value * obj.gain3.value * obj.gain3.value * 0.0001881737) - (obj.gain3.value * obj.gain3.value * obj.gain3.value * 0.02460543) + (obj.gain3.value * obj.gain3.value * 1.724025) - (obj.gain3.value * 71.53730));
-
-  const pick4gain = ((obj.gain4.value * obj.gain4.value * obj.gain4.value * obj.gain4.value * obj.gain4.value * obj.gain4.value * 0.000000001059022) - (obj.gain4.value * obj.gain4.value * obj.gain4.value * obj.gain4.value * obj.gain4.value * 0.0000007143797) + (obj.gain4.value * obj.gain4.value * obj.gain4.value * obj.gain4.value * 0.0001881737) - (obj.gain4.value * obj.gain4.value * obj.gain4.value * 0.02460543) + (obj.gain4.value * obj.gain4.value * 1.724025) - (obj.gain4.value * 71.53730));
-
-  const pick5gain = ((obj.gain5.value * obj.gain5.value * obj.gain5.value * obj.gain5.value * obj.gain5.value * obj.gain5.value * 0.000000001059022) - (obj.gain5.value * obj.gain5.value * obj.gain5.value * obj.gain5.value * obj.gain5.value * 0.0000007143797) + (obj.gain5.value * obj.gain5.value * obj.gain5.value * obj.gain5.value * 0.0001881737) - (obj.gain5.value * obj.gain5.value * obj.gain5.value * 0.02460543) + (obj.gain5.value * obj.gain5.value * 1.724025) - (obj.gain5.value * 71.53730));
-
-  if (pick1gain < 0) {
-    const a = pick1gain + 1959.184;
-    var b = Math.round(a);
-  } else {
-    var b = 0;
+    return 0;
   }
 
-  if (pick2gain < 0) {
-    const c = pick2gain + 1959.184;
-    var d = Math.round(c);
-  } else {
-    var d = 0;
-  }
-
-  if (pick3gain < 0) {
-    const e = pick3gain + 1959.184;
-    var f = Math.round(e);
-  } else {
-    var f = 0;
-  }
-
-  if (pick4gain < 0) {
-    const g = pick4gain + 1959.184;
-    var h = Math.round(g);
-  } else {
-    var h = 0;
-  }
-
-  if (pick5gain < 0) {
-    const i = pick5gain + 1959.184;
-    var j = Math.round(i);
-  } else {
-    var j = 0;
-  }
-
-  if (pick1lose < 0) {
-    const k = pick1lose + 1959.184;
-    var l = Math.round(k);
-  } else {
-    var l = 0;
-  }
-
-  if (pick2lose < 0) {
-    const m = pick2lose + 1959.184;
-    var n = Math.round(m);
-  } else {
-    var n = 0;
-  }
-
-  if (pick3lose < 0) {
-    const o = pick3lose + 1959.184;
-    var p = Math.round(o);
-  } else {
-    var p = 0;
-  }
-
-  if (pick4lose < 0) {
-    const q = pick4lose + 1959.184;
-    var r = Math.round(q);
-  } else {
-    var r = 0;
-  }
-
-  if (pick5lose < 0) {
-    const s = pick5lose + 1959.184;
-    var t = Math.round(s);
-  } else {
-    var t = 0;
-  }
-
-
-  const receivingValue = (b + d + f + h + j);
-  const givingValue = (l + n + p + r + t);
+  const receivingValue = receivingPicks.reduce((acc, pick) => {
+    return acc + getValue(pick);
+  }, 0);
+  const givingValue = givingPicks.reduce((acc, pick) => {
+    return acc + getValue(pick);
+  }, 0);
   let verdict;
   let difference = 0;
 
@@ -121,26 +50,7 @@ const doddsCalculator = (picks) => {
     givingPicks,
   } = picks;
 
-  const obj = {};
-  // create object shell that Doddsy required in 1997
-  // {
-  //   lose1: Number,
-  //   ..
-  //   lose5: Number,
-  //   gain1: Number,
-  //   ...
-  //   gain5: Number,
-  // }
-  for (var i = 0; i <= 4; i += 1) {
-    obj[`lose${i + 1}`] = {
-      value: (givingPicks[i]) ? givingPicks[i].overall : 0
-    };
-    obj[`gain${i + 1}`] = {
-      value: (receivingPicks[i]) ? receivingPicks[i].overall : 0
-    };
-  }
-
-  return calculate(obj);
+  return calculate(givingPicks, receivingPicks);
 }
 /* eslint-enable */
 export default doddsCalculator;
